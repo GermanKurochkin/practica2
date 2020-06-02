a5d5ed0b297d25ea6ba03b74a39458e600ed7b3b
Герман Курочкин <ggkurochkin@edu.hse.ru>
2020-06-02 12:13:07 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace practica2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int d, count = 0; ;
+            for(int a=1;a<=n/4;a++)
+            {
+                for (int b = a; b <= n / 3; b++)
+                {
+                    for (int c = b; c <= n / 2; c++)
+                    {
+                        d = n - a - b - c;
+                        if (d >= c) count++;
+                    }
+                }
+            }
+
+            Console.WriteLine(count);
+        }
+    }
+}
